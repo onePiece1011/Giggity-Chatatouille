9e1afc9d80eff143c90b20a622c09e12a50ea7ff
ManduIsmail <hannanismail08@gmail.com>
2026-02-12 10:02:44 +0100

    Update project_manager.py

--- project_manager.py
+++ project_manager.py
@@ -1,16 +1,35 @@
+from flask import render_template, request
 from models import Question
+from task_manager import calculate_score
+from utils import get_result
 
 def load_questions():
     return [
         Question(
-            question="Wat is de hoofdstad van Frankrijk?",
-            options=["Berlijn", "Parijs", "Rome"],
-            correct_answer=2
+            "Wat is de hoofdstad van Frankrijk?",
+            ["Berlijn", "Parijs", "Rome"],
+            "Parijs"
         ),
         Question(
-            question="Wie schreef de theorie van evolutie?",
-            options=["Newton", "Darwin", "Einstein"],
-            correct_answer=2
+            "Wie ontwikkelde de evolutietheorie?",
+            ["Darwin", "Newton", "Tesla"],
+            "Darwin"
         ),
-        # Vul aan tot 10
+        # maak dit 10
     ]
+
+def setup_routes(app):
+    questions = load_questions()
+
+    @app.route("/")
+    def index():
+        return render_template("index.html")
+
+    @app.route("/quiz", methods=["GET", "POST"])
+    def quiz():
+        if request.method == "POST":
+            score = calculate_score(questions, request.form)
+            result = get_result(score)
+            return render_template("result.html", score=score, result=result)
+
+        return render_template("quiz.html", questions=questions)
