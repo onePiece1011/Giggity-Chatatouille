8844a9811da31432afad2c656c5a9355331fc216
ManduIsmail <hannanismail08@gmail.com>
2026-02-12 09:55:22 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,15 +1,9 @@
-import time
-import sys
-
-def wait_for_spacebar_press(prompt: str = "Druk op spatie en houd vast..."):
-    print(prompt)
-    input("Druk op Enter om te beginnen met 'vasthouden' simuleren...")
-    start = time.time()
-    input("Laat Enter los om te stoppen (simuleert loslaten van spatie)...")
-    end = time.time()
-    duration = end - start
-    return duration
-
-def duration_to_power(duration: float, max_power: int = 100, max_seconds: float = 3.0) -> int:
-    ratio = min(duration / max_seconds, 1.0)
-    return int(ratio * max_power)
+def calculate_result(score: int) -> str:
+    if score <= 3:
+        return "Dom"
+    elif score <= 5:
+        return "Goed, maar kan beter"
+    elif score <= 8:
+        return "Super"
+    else:
+        return "Genie"
