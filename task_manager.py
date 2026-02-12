a5dea678dc55284d0e3a71d146e8362a095e5eb4
ManduIsmail <hannanismail08@gmail.com>
2026-02-12 10:08:54 +0100

    Update task_manager.py

--- task_manager.py
+++ task_manager.py
@@ -1,9 +1,10 @@
-from validators import valid_answer
+from validators import is_valid_answer
 
 def calculate_score(questions, form):
     score = 0
-    for i, q in enumerate(questions):
-        answer = form.get(f"q{i}")
-        if valid_answer(answer, q.options) and answer == q.correct:
-            score += 1
+    for index, question in enumerate(questions):
+        answer = form.get(f"q{index}")
+        if answer and is_valid_answer(answer, question.options):
+            if answer == question.correct:
+                score += 1
     return score
