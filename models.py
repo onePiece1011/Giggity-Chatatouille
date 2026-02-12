2e9533721e03596390b04a664174762a1d2e9a2e
ManduIsmail <hannanismail08@gmail.com>
2026-02-12 10:08:04 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 class Question:
-    def __init__(self, question, options, correct):
-        self.question = question
+    def __init__(self, text, options, correct):
+        self.text = text
         self.options = options
         self.correct = correct
